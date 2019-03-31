31675403aa149b783a739fc324f457dece70b580
tsuz <tak215@gmail.com>
2019-03-31 14:56:58 +0900

    adding more tests for linear regression

--- linereg_test.go
+++ linereg_test.go
@@ -9,90 +9,161 @@ import (
 )
 
 func TestLinReg(t *testing.T) {
+	interval := 300
+	max := 100
 	opts := SeriesOpts{
-		Interval: 300,
-		Max:      100,
+		Interval: interval,
+		Max:      max,
 	}
 	close := NewOHLCProp(OHLCPropClose)
-	linreg := NewLinReg(close, 3)
-	data := []OHLCV{
-		OHLCV{
-			C: 4119.05,
-		},
-		OHLCV{
-			C: 4119.18,
-		},
-		OHLCV{
-			C: 4118.02,
-		},
-		OHLCV{
-			C: 4118.01,
-		},
-		OHLCV{
-			C: 4118,
-		},
-		OHLCV{
-			C: 4113.35,
-		},
-	}
-	now := time.Now()
-	for i := range data {
-		data[i].S = now.Add(time.Duration(i*5) * time.Minute)
-	}
-	io := []struct {
-		output map[string]float64
+	linregname := "linreg"
+	datatable := []struct {
+		linereg int
+		io      []struct {
+			input  float64
+			output map[string]float64
+		}
 	}{
 		{
-			output: nil,
-		},
-		{
-			output: nil,
-		},
-		{
-			output: map[string]float64{
-				"linreg": 4118.235,
+			linereg: 3,
+			io: []struct {
+				input  float64
+				output map[string]float64
+			}{
+				{
+					input:  4119.05,
+					output: nil,
+				}, {
+					input:  4119.18,
+					output: nil,
+				}, {
+					input: 4118.02,
+					output: map[string]float64{
+						linregname: 4118.235,
+					},
+				}, {
+					input: 4118.01,
+					output: map[string]float64{
+						linregname: 4117.818,
+					},
+				}, {
+					input: 4118,
+					output: map[string]float64{
+						linregname: 4118,
+					},
+				}, {
+					input: 4113.35,
+					output: map[string]float64{
+						linregname: 4114.123,
+					},
+				},
 			},
 		},
 		{
-			output: map[string]float64{
-				"linreg": 4117.818,
-			},
-		},
-		{
-			output: map[string]float64{
-				"linreg": 4118,
-			},
-		},
-		{
-			output: map[string]float64{
-				"linreg": 4114.123,
+			linereg: 10,
+			io: []struct {
+				input  float64
+				output map[string]float64
+			}{
+				{
+					input:  0.00000628, // 2019/03/31 05:50
+					output: nil,
+				}, {
+					input:  0.00000631,
+					output: nil,
+				}, {
+					input:  0.00000631, // 2019/03/31 06:00
+					output: nil,
+				}, {
+					input:  0.00000631,
+					output: nil,
+				}, {
+					input:  0.00000631, // 2019/03/31 06:10
+					output: nil,
+				}, {
+					input:  0.00000632,
+					output: nil,
+				}, {
+					input:  0.00000633, // 2019/03/31 06:20
+					output: nil,
+				}, {
+					input:  0.00000632,
+					output: nil,
+				}, {
+					input:  0.00000632, // 2019/03/31 06:30
+					output: nil,
+				}, {
+					input: 0.00000633,
+					output: map[string]float64{
+						linregname: 0.0000063315,
+					},
+				}, {
+					input: 0.00000632, // 2019/03/31 06:40
+					output: map[string]float64{
+						linregname: 0.0000063267,
+					},
+				}, {
+					input: 0.00000631,
+					output: map[string]float64{
+						linregname: 0.0000063224,
+					},
+				}, {
+					input: 0.00000632, // 2019/03/31 06:50
+					output: map[string]float64{
+						linregname: 0.0000063215,
+					},
+				}, {
+					input: 0.00000629,
+					output: map[string]float64{
+						linregname: 0.0000063096,
+					},
+				}, {
+					input: 0.00000630, // 2019/03/31 07:00
+					output: map[string]float64{
+						linregname: 0.0000063024,
+					},
+				},
 			},
 		},
 	}
 
-	prettybad := 0.005
-	s, err := NewSeries(data, opts)
-	if err != nil {
-		t.Fatal(errors.Wrap(err, "error init series"))
-	}
-	s.AddIndicator("linreg", linreg)
-
-	name := "linreg"
-	for i, o := range io {
-		v := s.GetValueForInterval(data[i].S)
-		if v.Indicators[name] == nil && o.output == nil {
-			// nil value
-			continue
+	prettybad := 0.00001
+	name := linregname
+	for _, o := range datatable {
+		now := time.Now()
+		ohlcv := make([]OHLCV, 0)
+		for j, v := range o.io {
+			ohlcv = append(ohlcv, OHLCV{C: v.input, S: now.Add(time.Duration(j*interval) * time.Second)})
 		}
-		if v.Indicators[name] == nil && o.output == nil {
-			// value is nil for that name
-			continue
+		s, err := NewSeries(ohlcv, opts)
+		if err != nil {
+			t.Fatal(errors.Wrap(err, "error init series"))
 		}
-		if v.Indicators[name] == nil || o.output == nil {
-			t.Fatalf("expected both to be non nil but got %+v vs %+v at idx: %d for %s", v.Indicators[name], o.output, i, name)
+		linreg := NewLinReg(close, o.linereg)
+		err = s.AddIndicator(name, linreg)
+		if err != nil {
+			t.Fatal(errors.Wrap(err, "error adding indicator "))
 		}
-		if math.Abs(*(v.Indicators[name])/o.output[name]-1) >= prettybad {
-			t.Errorf("expected: %+v but got %+v for idx: %d for %s", (o.output)[name], *(v.Indicators[name]), i, name)
+		for j, o := range o.io {
+			if err != nil {
+				t.Fatal(errors.Wrap(err, "error init series"))
+			}
+			spectime := now.Add(time.Duration(j*interval) * time.Second)
+			v := s.GetValueForInterval(spectime)
+			if v.Indicators[name] == nil && o.output == nil {
+				// nil value
+				continue
+			}
+			if v.Indicators[name] == nil && o.output == nil {
+				// value is nil for that name
+				continue
+			}
+			if v.Indicators[name] == nil || o.output == nil {
+				t.Fatalf("expected both to be non nil but got %+v vs %+v at idx: %d for %s", v.Indicators[name], o.output, j, name)
+			}
+			if math.Abs(*(v.Indicators[name])/o.output[name]-1) >= prettybad {
+				t.Errorf("expected: %+v but got %+v for idx: %d for %s", (o.output)[name], *(v.Indicators[name]), j, name)
+			}
 		}
 	}
 }
