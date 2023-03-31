3cfc593af3ff3f24db5d2efc16eb84f9c8e55037
Taku Suzuki <tak215@gmail.com>
2023-04-01 03:51:49 +0900

    add div test

--- value_series_test.go
+++ value_series_test.go
@@ -23,3 +23,27 @@ func TestValueSeriesGetFirst(t *testing.T) {
 		t.Errorf("expected next value to be 2 but got  %+v", f.next.v)
 	}
 }
+
+func TestValueSeriesDiv(t *testing.T) {
+	a := NewValueSeries()
+	now := time.Now()
+	a.Set(now, 1)
+	a.Set(now.Add(time.Duration(1000*1e6)), 2)
+
+	b := NewValueSeries()
+	b.Set(now, 4)
+	b.Set(now.Add(time.Duration(1000*1e6)), 4)
+
+	c := a.Div(b)
+	c.SetCurrent(now)
+	f := c.GetCurrent()
+	if f == nil {
+		t.Fatalf("expected to be non nil but got nil")
+	}
+	if f.v != 0.25 {
+		t.Errorf("expected %+v but got %+v", 0.25, f.v)
+	}
+	if f.next.v != 0.5 {
+		t.Errorf("expected %+v but got %+v", 0.5, f.v)
+	}
+}
