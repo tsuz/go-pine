39e474eef349724bcc1b692521336c6b11898339
Taku Suzuki <tak215@gmail.com>
2023-04-15 17:00:41 +0900

    fix variance doc fmt

--- pine/series_variance.go
+++ pine/series_variance.go
@@ -10,25 +10,15 @@ import (
 // Variance generates a ValueSeries of variance.
 // Variance is the expectation of the squared deviation of a series from its mean (ta.sma, and it informally measures how far a set of numbers are spread out from their mean.
 //
-// Simplified formula is v = (1 / (N-1) * sum(xi - x)^2)
+// Simplified formula is
+//   - v = (1 / (N-1) * sum(xi - x)^2)
 //
 // Parameters
-// p - ValueSeries: source data
-// l - lookback: lookback periods [1, ∞)
+//   - p - ValueSeries: source data
+//   - l - lookback: lookback periods [1, ∞)
 //
 // TradingView's PineScript has an option to use an unbiased estimator, however; this function currently supports biased estimator.
 // Any effort to add a bias correction factor is welcome.
-//
-// Example:
-// t=time.Time        | 1   |  2  | 3   | 4    | 5   |
-// p=ValueSeries      | 13  | 15  | 11  | 19   | 21  |
-// sma(p, 3)	      | nil | nil | 13  | 15   | 17  |
-// p - sma(p, 3)(t=1) | nil | nil | nil | nil  | nil |
-// p - sma(p, 3)(t=2) | nil | nil | nil | nil  | nil |
-// p - sma(p, 3)(t=3) | 0   |  2  | -2  | 6    | 5   |
-// p - sma(p, 3)(t=4) | -2  |  0  | -4  | 4    | 6   |
-// p - sma(p, 3)(t=5) | -4  | -2  | -6  | 2    | 4   |
-// Variance(p, 3)     | nil | nil |  4  | 16   | 28  |
 func Variance(p ValueSeries, l int64) (ValueSeries, error) {
 	key := fmt.Sprintf("variance:%s:%d", p.ID(), l)
 	vari := getCache(key)
