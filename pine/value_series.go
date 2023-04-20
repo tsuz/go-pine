93f69bf7c893c471564c5f8294bb455743d8e406
Taku Suzuki <tak215@gmail.com>
2023-04-20 21:54:10 +0900

    handle nil values in arithmetics operations (#44)

--- pine/value_series.go
+++ pine/value_series.go
@@ -27,7 +27,6 @@ type ValueSeries interface {
 
 	Copy() ValueSeries
 
-	// replace existing
 	Set(time.Time, float64)
 	SetAll(val float64)
 
@@ -37,9 +36,10 @@ type ValueSeries interface {
 }
 
 type valueSeries struct {
-	id  string
-	cur *Value
-	ord []int64
+	id    string
+	cur   *Value
+	first *Value
+	last  *Value
 	sync.Mutex
 	timemap map[int64]*Value
 }
@@ -56,7 +56,6 @@ func NewValueSeries() ValueSeries {
 	u := uuid.NewV4()
 	v := &valueSeries{
 		id:      u.String(),
-		ord:     make([]int64, 0),
 		timemap: make(map[int64]*Value),
 	}
 	return v
@@ -79,8 +78,8 @@ func (s *valueSeries) Copy() ValueSeries {
 	return newv
 }
 
-func (s *valueSeries) Add(v ValueSeries) ValueSeries {
-	copied := s.Copy()
+func (s *valueSeries) operation(v ValueSeries, op func(a, b float64) float64) ValueSeries {
+	copied := NewValueSeries()
 	f := s.GetFirst()
 	for {
 		if f == nil {
@@ -88,111 +87,80 @@ func (s *valueSeries) Add(v ValueSeries) ValueSeries {
 		}
 		newv := v.Get(f.t)
 		if newv != nil {
-			copied.Set(f.t, f.v+newv.v)
+			copied.Set(f.t, op(f.v, newv.v))
 		}
 		f = f.next
+	}
+	cur := s.GetCurrent()
+	if cur != nil {
+		copied.SetCurrent(cur.t)
 	}
 	return copied
 }
 
-func (s *valueSeries) AddConst(v float64) ValueSeries {
-	copied := s.Copy()
+func (s *valueSeries) operationConst(op func(a float64) float64) ValueSeries {
+	copied := NewValueSeries()
 	f := s.GetFirst()
 	for {
 		if f == nil {
 			break
 		}
-		copied.Set(f.t, f.v+v)
+		copied.Set(f.t, op(f.v))
 		f = f.next
 	}
+	cur := s.GetCurrent()
+	if cur != nil {
+		copied.SetCurrent(cur.t)
+	}
 	return copied
+}
+
+func (s *valueSeries) Add(v ValueSeries) ValueSeries {
+	return s.operation(v, func(a, b float64) float64 {
+		return a + b
+	})
+}
+
+func (s *valueSeries) AddConst(c float64) ValueSeries {
+	return s.operationConst(func(a float64) float64 {
+		return a + c
+	})
 }
 
 func (s *valueSeries) Div(v ValueSeries) ValueSeries {
-	copied := s.Copy()
-	f := s.GetFirst()
-	for {
-		if f == nil {
-			break
-		}
-		newv := v.Get(f.t)
-		if newv != nil {
-			copied.Set(f.t, f.v/newv.v)
-		}
-		f = f.next
-	}
-	return copied
+	return s.operation(v, func(a, b float64) float64 {
+		return a / b
+	})
 }
 
 func (s *valueSeries) DivConst(v float64) ValueSeries {
-	copied := s.Copy()
-	f := s.GetFirst()
-	for {
-		if f == nil {
-			break
-		}
-		copied.Set(f.t, f.v/v)
-		f = f.next
-	}
-	return copied
+	return s.operationConst(func(a float64) float64 {
+		return a / v
+	})
 }
 
 func (s *valueSeries) Mul(v ValueSeries) ValueSeries {
-	copied := s.Copy()
-	f := s.GetFirst()
-	for {
-		if f == nil {
-			break
-		}
-		newv := v.Get(f.t)
-		if newv != nil {
-			copied.Set(f.t, f.v*newv.v)
-		}
-		f = f.next
-	}
-	return copied
+	return s.operation(v, func(a, b float64) float64 {
+		return a * b
+	})
 }
 
 func (s *valueSeries) MulConst(v float64) ValueSeries {
-	copied := NewValueSeries()
-	f := s.GetFirst()
-	for {
-		if f == nil {
-			break
-		}
-		copied.Set(f.t, f.v*v)
-		f = f.next
-	}
-	return copied
+	return s.operationConst(func(a float64) float64 {
+		return a * v
+	})
 }
 
 func (s *valueSeries) Sub(v ValueSeries) ValueSeries {
-	copied := NewValueSeries()
-	f := s.GetFirst()
-	for {
-		if f == nil {
-			break
-		}
-		newv := v.Get(f.t)
-		if newv != nil {
-			copied.Set(f.t, f.v-newv.v)
-		}
-		f = f.next
-	}
-	return copied
+	return s.operation(v, func(a, b float64) float64 {
+		return a - b
+	})
 }
 
 func (s *valueSeries) SubConst(v float64) ValueSeries {
-	copied := s.Copy()
-	f := s.GetFirst()
-	for {
-		if f == nil {
-			break
-		}
-		copied.Set(f.t, f.v-v)
-		f = f.next
-	}
-	return copied
+	return s.operationConst(func(a float64) float64 {
+		return a - v
+	})
 }
 
 func (s *valueSeries) ID() string {
@@ -202,6 +170,7 @@ func (s *valueSeries) ID() string {
 func (s *valueSeries) SetCurrent(t time.Time) bool {
 	v, ok := s.timemap[t.Unix()]
 	if !ok {
+		s.cur = nil
 		return false
 	}
 	s.cur = v
@@ -213,20 +182,11 @@ func (s *valueSeries) GetCurrent() *Value {
 }
 
 func (s *valueSeries) GetFirst() *Value {
-	if len(s.ord) == 0 {
-		return nil
-	}
-
-	val := s.getValue(s.ord[0])
-
-	return val
+	return s.first
 }
 
 func (s *valueSeries) GetLast() *Value {
-	if len(s.ord) == 0 {
-		return nil
-	}
-	return s.getValue(s.ord[len(s.ord)-1])
+	return s.last
 }
 
 func (s *valueSeries) Val() *float64 {
@@ -242,11 +202,6 @@ func (s *valueSeries) Get(t time.Time) *Value {
 
 func (s *valueSeries) getValue(t int64) *Value {
 	return s.timemap[t]
-}
-
-func (s *valueSeries) appendValue(v *Value) {
-	s.ord = append(s.ord, v.t.Unix())
-	s.setValue(v.t.Unix(), v)
 }
 
 func (s *valueSeries) setValue(t int64, v *Value) {
@@ -265,14 +220,14 @@ func (s *valueSeries) SetAll(val float64) {
 	}
 }
 
-// Push will append at the end of the list. Replaces value if exists
+// Set appends to the end of the series. If same timestamp exists, its value will be replaced
 func (s *valueSeries) Set(t time.Time, val float64) {
 	curval := s.getValue(t.Unix())
 	if curval != nil {
-		// just replace the map
+		// replace existing
 		v2 := &Value{
-			next: s.timemap[t.Unix()].next,
-			prev: s.timemap[t.Unix()].prev,
+			next: curval.next,
+			prev: curval.prev,
 			t:    t,
 			v:    val,
 		}
@@ -282,8 +237,14 @@ func (s *valueSeries) Set(t time.Time, val float64) {
 		if curval.next != nil {
 			curval.next.prev = v2
 		}
-		if s.cur != nil && s.cur.t.Equal(t) {
+		if s.cur == curval {
 			s.cur = v2
+		}
+		if s.first == curval {
+			s.first = v2
+		}
+		if s.last == curval {
+			s.last = v2
 		}
 		s.setValue(t.Unix(), v2)
 		return
@@ -293,19 +254,13 @@ func (s *valueSeries) Set(t time.Time, val float64) {
 		t: t,
 		v: val,
 	}
-
-	// no existing values so no previous or next pointers
-	if len(s.ord) == 0 {
-		s.appendValue(v)
-		return
+	if s.last != nil {
+		s.last.next = v
+		v.prev = s.last
 	}
-
-	prevt := s.ord[len(s.ord)-1]
-	prev := s.timemap[prevt]
-	v.prev = prev
-
-	s.appendValue(v)
-	curt := s.getValue(v.t.Unix())
-
-	s.timemap[prevt].next = curt
+	s.last = v
+	if s.first == nil {
+		s.first = v
+	}
+	s.setValue(t.Unix(), v)
 }
