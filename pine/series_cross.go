c25aadd25d52f2784c4e159d558a4077db55b992
Taku Suzuki <tak215@gmail.com>
2023-04-27 21:34:12 +0900

    add cross

---
+++ pine/series_cross.go
@@ -0,0 +1,27 @@
+package pine
+
+// Cross generates ValueSeries of ketler channel's middle, upper and lower in that order.
+func Cross(a, b ValueSeries) (ValueSeries, error) {
+	c := a.OperateWithNil(b, func(av, bv *Value) *Value {
+		if av == nil || bv == nil {
+			return nil
+		}
+		zero := &Value{
+			t: av.t,
+			v: 0,
+		}
+		if av.prev == nil || bv.prev == nil {
+			return zero
+		}
+		if av.v < bv.v && av.prev.v > bv.prev.v ||
+			av.v > bv.v && av.prev.v < bv.prev.v {
+			return &Value{
+				t: av.t,
+				v: 1.0,
+			}
+		}
+		return zero
+	})
+
+	return c, nil
+}
